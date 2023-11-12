3c344bb35d6f801bd736f5509df9e2f737417cc2
Tuxce <marcioncosta@gmail.com>
2023-11-11 22:47:55 -0300

    Configuração inicial

---
+++ backend/appback/src/main/java/br/com/pigrp17/appback/AppbackApplication.java
@@ -0,0 +1,13 @@
+package br.com.pigrp17.appback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbackApplication.class, args);
+	}
+
+}
