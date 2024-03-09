52c79e572fe16f55d1c9a191bea312a5b2ba884a
Isabella Macedo <isabellamacedocontato@gmail.com>
2024-03-09 17:28:24 -0300

    [FRONT-END] Limpeza de código, criação das rotas e páginas

---
+++ frontend/terra-trocada/src/pages/cadastro.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar";
+
+export default function Cadastro() {
+  return (
+    //Essa tag vazia é chamada de fragmento. Os fragmentos permitem agrupar coisas sem deixar rastros na árvore HTML do navegador. https://react.dev/learn/writing-markup-with-jsx
+    <>
+      <Navbar />
+      <h1>Cadastro</h1>
+    </>
+  );
+}
---
+++ frontend/terra-trocada/src/pages/quem-somos.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar";
+
+export default function QuemSomos() {
+  return (
+    //Essa tag vazia é chamada de fragmento. Os fragmentos permitem agrupar coisas sem deixar rastros na árvore HTML do navegador. https://react.dev/learn/writing-markup-with-jsx
+    <>
+      <Navbar />
+      <h1>Quem Somos</h1>
+    </>
+  );
+}
