ebdaeb359525c24e8d2fe1f0324d50aa80614aee
Isabella Macedo <isabellamacedocontato@gmail.com>
2024-03-09 22:08:05 -0300

    [FRONT-END] Instalação do SASS, customização cores, logotipo e navbar

--- frontend/terra-trocada/src/components/navbar.tsx
+++ frontend/terra-trocada/src/components/navbar.tsx
@@ -1,29 +1,43 @@
 import React from 'react';
 import Link from 'next/link';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useRouter } from 'next/router';
 
 const NavbarLinks = () => {
+  // Path atual da rota
+  const { pathname } = useRouter();
+
   return (
-    <Navbar bg="light" data-bs-theme="light">
+    <Navbar bg="" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Link href="/" className="nav-link">
-            Home
-          </Link>
-          <Link href="/cadastro" className="nav-link">
-            Cadastro
-          </Link>
-          <Link href="/contato" className="nav-link">
-            Contato
-          </Link>
-          <Link href="/proposito" className="nav-link">
-            Nosso Propósito
-          </Link>
-          <Link href="/quem-somos" className="nav-link">
-            Quem Somos
-          </Link>
-        </Nav>
+        <Navbar.Brand href="/" className="d-flex align-items-center">
+          <img
+            src="/img/logotipo.png"
+            alt="Terra Trocada"
+            width="150"
+            className="mr-3"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Link href="/" className={`nav-link ${pathname === '/' ? 'active' : ''}`}>
+              Home
+            </Link>
+            <Link href="/cadastro" className={`nav-link ${pathname === '/cadastro' ? 'active' : ''}`}>
+              Cadastro
+            </Link>
+            <Link href="/contato" className={`nav-link ${pathname === '/contato' ? 'active' : ''}`}>
+              Contato
+            </Link>
+            <Link href="/proposito" className={`nav-link ${pathname === '/proposito' ? 'active' : ''}`}>
+              Nosso Propósito
+            </Link>
+            <Link href="/quem-somos" className={`nav-link ${pathname === '/quem-somos' ? 'active' : ''}`}>
+              Quem Somos
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
--- frontend/terra-trocada/src/pages/_app.tsx
+++ frontend/terra-trocada/src/pages/_app.tsx
@@ -1,6 +1,4 @@
-// Para não precisar importar todas as vezes o bootstrap e as variáveis globais, centralizo aqui esses imports.
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/globals.css';
+import '../styles/globals.scss';
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
